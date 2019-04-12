22f4ce3e328285e11b272579b29bd4c9177c1afd
Adalberto Fernandes Jr <adalberto.fernandesjr@gmail.com>
2019-04-11 21:26:44 -0300

    First commit

---
+++ app/src/main/java/br/com/adalbertofjr/marvelheroes/Characters/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.com.adalbertofjr.marvelheroes.Characters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.adalbertofjr.marvelheroes.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
